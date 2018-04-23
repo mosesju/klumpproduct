59c1704b26625d8a0046cb64ac1ac9eecca9c81c
Michael Pedzimaz <mpedzi03@gmail.com>
2018-04-23 17:51:01 -0500

    second commit

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ var app = express();
 var port = process.env.PORT || 3000;
 app.get('/',(req, res) => res.send('Hello World'));
 app.listen (port, () => console.log('Server is running on port'+ port));
+
+var urlsEach = {
+    "https://michael-pedzimaz-webapp.azurewebsites.net/my-information.json":"./files/Mike/my-information.json",
+    "https://jakeisalsoclueless.azurewebsites.net/myinformation.json":"./files/Jake/my-information.json",
+    "https://riotjuice.azurewebsites.net/my-information.json":"./files/Julian/my-information.json",
+    "https://softwareengjmh.azurewebsites.net/format.JSON":"./files/Jace/my-information.json",
+    "https://newtestdocument.azurewebsites.net/Format.json":"./files/Thad/my-information.json"
+}
+var groupFile = './files/Group/group-information'
